33036124e3385b447f7656e5919f662e4cdf127b
RobinStut <robin.stut@hva.nl>
2019-03-12 14:46:07 +0100

    added all routes + css

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(express.static('static'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('pages/index'))
+app.get('/login', (req, res) => res.render('pages/login'))
+app.get('/patientOverview', (req, res) => res.render('pages/patientOverview'))
+app.get('/patientVanMaatje', (req, res) => res.render('pages/patientVanMaatje'))
+app.get('/maatjeVanPatient', (req, res) => res.render('pages/maatjeVanPatient'))
+app.get('/lijstSamenstellen', (req, res) => res.render('pages/lijstSamenstellen'))
+app.get('/login', (req, res) => res.render('pages/maatjeOverview'))
+
 app.get('/test', (req, res) => res.send('test!'))
 app.get('/case/:caseId', (req, res) => {
   res.send(req.params.caseId)
